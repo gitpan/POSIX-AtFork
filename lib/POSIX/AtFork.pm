482ef2d012cec6ca66690c6d0e9d78bf4565dabd
藤 吾郎 <gfuji@cpan.org>
2010-08-19 08:12:39 +0000

    Import of GFUJI/POSIX-AtFork-0.01_01 from CPAN.
    gitpan-cpan-distribution: POSIX-AtFork
    gitpan-cpan-version:      0.01_01
    gitpan-cpan-path:         GFUJI/POSIX-AtFork-0.01_01.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     developer

---
+++ lib/POSIX/AtFork.pm
@@ -0,0 +1,66 @@
+package POSIX::AtFork;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01_01';
+
+require Exporter;
+our @ISA = qw(Exporter);
+
+our @EXPORT_OK   = qw(pthread_atfork);
+our %EXPORT_TAGS = (ALL => \@EXPORT_OK);
+
+require XSLoader;
+XSLoader::load('POSIX::AtFork', $VERSION);
+
+
+1;
+__END__
+
+=head1 NAME
+
+POSIX::AtFork - Hook registrations at fork(2)
+
+=head1 SYNOPSIS
+
+  # POSIX interface:
+  use POSIX::AtFork qw(pthread_atfork);
+  
+  pthread_atfork(\&prepare, \&parent, \&child);
+
+  # or per-hook interfaces:
+  POSIX::AtFork->add_to_prepare(\&prepare);
+  POSIX::AtFork->add_to_parent(\&parent);
+  POSIX::AtFork->add_to_child(\&child);
+
+  # registered callbacks can be removed
+  POSIX::AtFork->delete_from_prepare(\&prepare);
+  POSIX::AtFork->delete_from_parent( \&parent);
+  POSIX::AtFork->delete_from_child(  \&child);
+
+=head1 DESCRIPTION
+
+This module is an interface to C<pthread_atfork(3)> which registeres
+handlers called before and after C<fork(2)>.
+
+=head2 INTERFACE
+
+See SYNOPSIS.
+
+=head1 SEE ALSO
+
+L<pthread_atfork(3)>.
+
+=head1 AUTHOR
+
+Fuji, Goro (gfx)
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Fuji, Goro gfx E<lt>gfuji@cpan.orgE<gt>. 
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself,
+
+=cut
